3e03e06e30258fa4c966b93646a5bc8cb89ef9f8
walisson27 <you@example.com>
2022-07-11 11:58:31 -0300

    first heard

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import * as C from "./styles";
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Title>Finanças</C.Title>
+      </C.Header>
+        <C.Subtitle>© Walisson Souza, 2022</C.Subtitle>
+    </C.Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #4682b4;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
+
+export const Subtitle = styled.div`
+  font-size: 20px;
+  color: white;
+`;
